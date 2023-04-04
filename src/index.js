9c32c7a20a340f2e17fc5401173668edf7b742d2
Chirkov Slava <slavachirkov92@gmail.com>
2023-04-04 21:21:42 +0300

    add script-start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { UserService } from "./modules/userService"
-import { render, renderError } from "./modules/render"
+import { render } from "./modules/render"
 import { addUsers } from "./modules/addUsers"
 import { removeUsers } from "./modules/removeUsers"
 import { changePermissions } from "./modules/changePermissions"
@@ -9,9 +9,7 @@ import { sortUsers } from "./modules/sortUsers"
 import { searchUsers } from "./modules/searchUsers"
 
 window.userService = new UserService
-userService.getUsers()
-    .then(data => render(data))
-// .catch((error) => renderError())
+userService.getUsers().then(data => render(data))
 
 addUsers()
 removeUsers()
